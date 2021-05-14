9cdce24491244479dc40c15d8d38bd34c7d36303
skycity20180911 <skycity20180911@outlook.com>
2021-05-14 21:11:19 +0800

    8

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = var.aws_region
-
+  assume_role {
+    role_arn = "arn:aws-cn:iam::815347205985:role/OrgAccAccessAdminRole"
+    session_name = "terraform"
+  }
 }
 
 provider "random" {}
