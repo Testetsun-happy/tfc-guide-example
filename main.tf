b282369ab5fea2530da78a5ef9ddf7611f23924a
Sunxu <xu.sun@yidatec.com>
2021-05-15 22:37:43 +0800

    27

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-module dms {
+module endpoint_source {
   source = "./modules/dms" 
   application = "${var.application}"
   env = "${var.env}"
@@ -10,6 +10,20 @@ module dms {
   database = "prod_0516"
   port = 1433
   username = "Syngenta-cms-prod"
+  password="123456"
+}
+
+module endpoint_target {
+  source = "./modules/dms" 
+  application = "${var.application}"
+  env = "${var.env}"
+  // to be
+  name_suffix = "aws-prd-0516"
+  server= "db-miniprogram-prod-m5-2x-8c32g-std.cjz5junphtwg.rds.cn-northwest-1.amazonaws.com.cn"
+  // to be
+  database = "cmsprod"
+  port = 1433
+  username = "syngenta_cms_prd"
   password="123456"
 }
 
--- modules/dms/main.tf
+++ modules/dms/main.tf
@@ -17,3 +17,22 @@ resource "aws_dms_endpoint" "source_sqlserver" {
 
   username = "${var.username}"
 }
+
+resource "aws_dms_endpoint" "target_sqlserver" {
+  database_name               = "${var.database}"
+  endpoint_id                 = "dms-target-${var.application}-${var.env}-${var.name_suffix}"
+  endpoint_type               = "target"
+  engine_name                 = "sqlserver"
+  extra_connection_attributes = ""
+  password                    = "${var.password}"
+  port                        = var.port
+  server_name                 = "${var.server}"
+  ssl_mode                    = var.enable_ssl
+
+  tags = {
+    Name = "dms-target-${var.application}-${var.env}-${var.name_suffix}"
+  }
+
+  username = "${var.username}"
+}
+
