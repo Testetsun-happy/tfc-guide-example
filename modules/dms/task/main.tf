2df3894e04aa277ad66b27684791c3688e2e8592
Sunxu <xu.sun@yidatec.com>
2021-05-23 22:07:39 +0800

    57

--- modules/dms/task/main.tf
+++ modules/dms/task/main.tf
@@ -129,8 +129,7 @@ resource "aws_dms_replication_task" "dms-task-main" {
                 "Severity": "LOGGER_SEVERITY_DEFAULT"
             }
         ],
-        "CloudWatchLogGroup": "${aws_cloudwatch_log_group.task_log.name}",
-        "CloudWatchLogStream": "${aws_cloudwatch_log_stream.task_log_stream.id}"
+        "CloudWatchLogGroup": "${aws_cloudwatch_log_group.task_log.name}"
     },
     "ControlTablesSettings": {
         "historyTimeslotInMinutes": 5,
